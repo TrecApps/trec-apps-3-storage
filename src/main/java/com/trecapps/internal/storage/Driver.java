67c9de2cd17a1535dbbf8a31148076a080363558
John Jacko <jljacko@outlook.com>
2021-11-24 11:50:32 -0600

    Added Logging message to Driver class

--- src/main/java/com/trecapps/internal/storage/Driver.java
+++ src/main/java/com/trecapps/internal/storage/Driver.java
@@ -3,10 +3,18 @@ package com.trecapps.internal.storage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class Driver {
+
+    static Logger logger = Logger.getLogger(Driver.class.toString());
+
     public static void main(String[] args)
     {
+        logger.log(Level.INFO, "About to launch spring!");
         SpringApplication.run(Driver.class, args);
+        logger.log(Level.INFO, "Launched spring!");
     }
 }
